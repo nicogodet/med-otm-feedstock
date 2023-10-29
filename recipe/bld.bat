e524dbe7d15764bc6391098dbbdeeeade73c4954
nicogodet <nicolas.godet@outlook.fr>
2023-10-29 21:07:22 +0100

    build with hdf5-1.14.2

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,6 @@
 :: https://github.com/scivision/mumps/blob/main/Readme_Windows.md
 :: Generate build files
+set CXXFLAGS=-std=c++11
 cmake -G "MinGW Makefiles" ^
       -B build ^
       -D CMAKE_BUILD_TYPE:STRING=RELEASE ^
@@ -9,13 +10,14 @@ cmake -G "MinGW Makefiles" ^
       -D MEDFILE_INSTALL_DOC:BOOL=OFF ^
       -D MEDFILE_BUILD_SHARED_LIBS:BOOL=ON ^
       -D MEDFILE_BUILD_STATIC_LIBS:BOOL=OFF ^
-      -D MEDFILE_USE_MPI:BOOL=ON
+      -D MEDFILE_USE_MPI:BOOL=ON ^
+      -D H5_USE_110_API:BOOL=ON
 
 :: Build
 cmake --build build --parallel
 cmake --install build
 
-DEL /q %PREFIX%\mingw-w64\bin\mdump %PREFIX%\mingw-w64\bin\xmdump
-COPY /y %LIBRARY_PREFIX%\mingw-w64\bin\mdump4.exe %LIBRARY_PREFIX%\mingw-w64\bin\mdump.exe
-COPY /y %LIBRARY_PREFIX%\mingw-w64\bin\xmdump4.exe %LIBRARY_PREFIX%\mingw-w64\bin\xmdump.exe
-MOVE %LIBRARY_PREFIX%\mingw-w64\lib\*.dll %LIBRARY_PREFIX%\mingw-w64\bin\
+del /q %PREFIX%\mingw-w64\bin\mdump %PREFIX%\mingw-w64\bin\xmdump
+copy /y %LIBRARY_PREFIX%\mingw-w64\bin\mdump4.exe %LIBRARY_PREFIX%\mingw-w64\bin\mdump.exe
+copy /y %LIBRARY_PREFIX%\mingw-w64\bin\xmdump4.exe %LIBRARY_PREFIX%\mingw-w64\bin\xmdump.exe
+move %LIBRARY_PREFIX%\mingw-w64\lib\*.dll %LIBRARY_PREFIX%\mingw-w64\bin\
